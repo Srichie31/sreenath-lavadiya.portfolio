45b825cc81e4f800ce8e31bf91b7b299f55407d9
L Sreenath <98385769+Sreenath31@users.noreply.github.com>
2024-07-11 03:26:46 +0530

    added skills tab

--- frontend/src/app/skills/skills.component.ts
+++ frontend/src/app/skills/skills.component.ts
@@ -5,6 +5,23 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 
+import {
+  faAngular,
+  faPython,
+  faJava,
+  faJs,
+  faHtml5,
+  faCss3,
+  faSass,
+  faBootstrap,
+  faNodeJs,
+  faAws,
+  faAndroid,
+  faGithub
+  
+} from '@fortawesome/free-brands-svg-icons';
+import skills from '../../assets/data/skills.json';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -12,9 +29,37 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class SkillsComponent implements OnInit {
+  icon = faGithub;
+  sBtn : any = 'a'
+  icons:any = {
+    angular: faAngular,
+    python: faPython,
+    java: faJava,
+    js: faJs,
+    html : faHtml5,
+    css : faCss3,
+    sass : faSass,
+    bootstrap : faBootstrap,
+    node : faNodeJs,
+    git:faGithub
+  };
+  skills: any = skills;
+  allSkills: any = [];
+  obj: any;
+  iconKeys : any = Object.keys(this.icons)
 
   constructor() {}
 
-
-  ngOnInit(): void{}
+  ngOnInit(): void {
+    this.allSkills = skills;
+    console.log(this.allSkills);
+  }
+  filter(str: any) {
+    this.sBtn = str
+    if (str === 'a') {
+      this.allSkills = skills;
+    } else {
+      this.allSkills = skills.filter((obj) => obj.type == str);
+    }
+  }
 }
